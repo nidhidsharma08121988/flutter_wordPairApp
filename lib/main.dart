6a6033a0eef72bbf7f1093f34e96a54621b8eb97
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2023-11-23 10:59:36 +0400

    Display Test Title on AppBar

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  // TODO: implement key
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+          ),
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text('Hello from us'),
+          ),
+        ));
+  }
+}
