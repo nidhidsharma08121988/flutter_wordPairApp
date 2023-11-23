09eb6ec53035aa2f9fbfccb1f6d43d281d71a470
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2023-11-23 13:47:13 +0400

    Update List padding for each list item(list tile)

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,7 @@ class MyApp extends StatelessWidget {
     final List<WordPair> randomWordPairs = <WordPair>[];
 
     return ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            color: Colors.amber[colorCodes[index]],
-            child: Center(child: wordPairGenerator()),
+        padding: const EdgeInsets.all(16),
           );
         });
   }
