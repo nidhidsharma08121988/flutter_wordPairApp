75e129a7d7d3c626f69e6718595b1348ed3bca04
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2023-11-27 14:54:17 +0400

    Create Stateful Widget

---
+++ lib/app_body.dart
@@ -0,0 +1,8 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class AppBody extends StatefulWidget {
+  const AppBody({super.key});
+  @override
+  BodyState createState() => BodyState();
+}
