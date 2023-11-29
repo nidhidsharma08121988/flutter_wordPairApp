789637586152e7dad70fe2a15bb81566f8322b41
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2023-11-29 16:56:41 +0400

    Create favoriteWords Widget for favorite words

---
+++ lib/favorite_words.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class FavoriteWords extends StatefulWidget {
+  final Set<String> favorites;
+  const FavoriteWords({super.key, this.favorites = const {}});
+
+  @override
+  _FavoriteWordsState createState() => _FavoriteWordsState();
+}
+
+class _FavoriteWordsState extends State<FavoriteWords> {
+  @override
+  Widget build(BuildContext context) {
+    final Iterable<ListTile> tiles = widget.favorites.map((tile) {
+      return ListTile(
+        title: Text(tile),
+      );
+    });
+    final List<Widget> dividedTiles =
+        ListTile.divideTiles(tiles: tiles, context: context).toList();
+    return Scaffold(
+        appBar: AppBar(title: const Text('Saved favorites')),
+        body: ListView(children: [...dividedTiles]));
+  }
+}
