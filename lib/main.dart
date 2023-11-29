c5a16644971f0230249d13fe0c29d3e649167abe
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2023-11-29 11:07:49 +0400

    Remove new screen Since this change requires to maintain a app level state of favorites We undo the actions

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,6 @@ import 'app_body.dart';
 
 void main() => runApp(const MyApp());
 
-class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -27,20 +18,9 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
             appBar: AppBar(
-                centerTitle: true,
-                title: const Text('Random Words Generator'),
-                actions: <Widget>[
-                  IconButton(
-                    icon: const Icon(Icons.list),
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                const FavoriteScreen()),
-                      );
-                    },
-                  )
-                ]),
+              centerTitle: true,
+              title: const Text('Random Words Generator'),
+            ),
             body: const AppBody()));
   }
 }
